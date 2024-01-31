ed2e2e3604692f763226fe8dc047aabf4e8df90c
Isa Kilic <Is4Kilic@hotmail.com>
2024-01-31 21:10:20 +0100

    refactor/ can now view individual ingredients in nutrition information onclick

--- app/src/main/java/com/kidrich/turbotrack/MealNutritionActivity.kt
+++ app/src/main/java/com/kidrich/turbotrack/MealNutritionActivity.kt
@@ -2,7 +2,6 @@ package com.kidrich.turbotrack
 
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.Entry
@@ -18,6 +17,10 @@ import com.kidrich.turbotrack.databinding.ActivityMealNutritionsBinding
 class MealNutritionActivity: AppCompatActivity(), OnChartValueSelectedListener {
 
     private lateinit var binding: ActivityMealNutritionsBinding
+    private val proteinList: ArrayList<Pair<Ingredient, Double>> = arrayListOf()
+    private val fatList: ArrayList<Pair<Ingredient, Double>> = arrayListOf()
+    private val sugarList: ArrayList<Pair<Ingredient, Double>> = arrayListOf()
+    private val saltList: ArrayList<Pair<Ingredient, Double>> = arrayListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -42,21 +45,40 @@ class MealNutritionActivity: AppCompatActivity(), OnChartValueSelectedListener {
             binding.mealNutritionalInfoMealcals.text = mealCals + " calories total"
             binding.mealNutritionalInfoMealname.text = mealName
 
-
-            val totalFat = receivedList.sumOf {
-                ((it.fat100g?.div(100) ?: 0.0 / 100) * (it.grams))
+            receivedList.forEach {
+                if (it.fat100g != 0.0 && it.fat100g != null && it.fat100g >= 0.5) {
+                    val fatAmount = (((it.fat100g.div(100) ?: (0.0 / 100)) * (it.grams)))
+                    fatList.add(Pair(it, fatAmount))
+                }
+                if (it.sugars100g != 0.0 && it.sugars100g != null && it.sugars100g >= 1) {
+                    val sugarAmount = (((it.sugars100g.div(100) ?: (0.0 / 100)) * (it.grams)))
+                    sugarList.add(Pair(it, sugarAmount))
+                }
+                if (it.salt100g != 0.0 && it.salt100g != null && it.salt100g >= 0.01) {
+                    val saltAmount = (((it.salt100g.div(100) ?: (0.0 / 100)) * (it.grams)))
+                    saltList.add(Pair(it, saltAmount))
+                }
+                if (it.proteins100g != 0.0 && it.proteins100g != null && it.proteins100g >= 0.3) {
+                    val proteinAmount = (((it.proteins100g.div(100) ?: (0.0 / 100)) * (it.grams)))
+                    proteinList.add(Pair(it, proteinAmount))
+                }
             }
 
-            val totalSugar = receivedList.sumOf {
-                ((it.sugars100g?.div(100) ?: 0.0 / 100) * (it.grams))
+
+            val totalFat = fatList.sumOf {
+                it.second
             }
 
-            val totalSalt = receivedList.sumOf {
-                ((it.salt100g?.div(100) ?: 0.0 / 100) * (it.grams))
+            val totalSugar = sugarList.sumOf {
+                it.second
             }
 
-            val totalProtein = receivedList.sumOf {
-                ((it.proteins100g?.div(100) ?: 0.0 / 100) * (it.grams))
+            val totalSalt = saltList.sumOf {
+                it.second
+            }
+
+            val totalProtein = proteinList.sumOf {
+                it.second
             }
 
             // Customize the chart appearance
@@ -71,23 +93,23 @@ class MealNutritionActivity: AppCompatActivity(), OnChartValueSelectedListener {
             val entries: ArrayList<PieEntry> = arrayListOf()
 
 
-            if(totalFat.toFloat() > 0.2f) {
+            if(totalFat.toFloat() > 0.5f) {
                 entries.add(PieEntry(totalFat.toFloat(), "Fat", ))
             }
 
-            if(totalSugar.toFloat() > 0.2f) {
+            if(totalSugar.toFloat() > 0.8f) {
                 entries.add(PieEntry(totalSugar.toFloat(), "Sugar"))
             }
 
-            if(totalSalt.toFloat() > 0.2f) {
+            if(totalSalt.toFloat() > 0.4f) {
                 entries.add(PieEntry(totalSalt.toFloat(), "Salt"))
             }
 
-            if(totalProtein.toFloat() > 0.2f) {
+            if(totalProtein.toFloat() > 0.8f) {
                 entries.add(PieEntry(totalProtein.toFloat(), "Protein"))
             }
 
-            val dataSet: PieDataSet = PieDataSet(entries, "Meal-Nutrients")
+            val dataSet: PieDataSet = PieDataSet(entries, "")
             dataSet.colors = ColorTemplate.COLORFUL_COLORS.toList();
             dataSet.valueTextColor = Color.WHITE
             dataSet.valueTextSize = 15f
@@ -115,14 +137,41 @@ class MealNutritionActivity: AppCompatActivity(), OnChartValueSelectedListener {
         binding.mealNutritionCloseButton.setOnClickListener {
             finish()
         }
-
-
-
     }
 
     override fun onValueSelected(e: Entry?, h: Highlight?) {
         if (e is PieEntry) {
-            binding.mealNutritionalInfoSpecificText.text = e.label + " - " + e.value + "g"
+            when(e.label) {
+                "Sugar" -> {
+                    binding.mealNutritionalInfoSpecificText.text = e.value.toString() + "g total sugar\n\n"
+                    sugarList.forEach {
+                        binding.mealNutritionalInfoSpecificText.text =
+                            binding.mealNutritionalInfoSpecificText.text.toString() + it.first.name + " - " + it.second.toString() + "g\n"
+                    }
+                }
+                "Fat" -> {
+                    binding.mealNutritionalInfoSpecificText.text = e.value.toString() + "g total fat\n\n"
+                    fatList.forEach {
+                        binding.mealNutritionalInfoSpecificText.text =
+                            binding.mealNutritionalInfoSpecificText.text.toString() + it.first.name + " - " + it.second.toString() + "g\n"
+                    }
+                }
+                "Protein" -> {
+                    binding.mealNutritionalInfoSpecificText.text = e.value.toString() + "g total protein\n\n"
+                    proteinList.forEach {
+                        binding.mealNutritionalInfoSpecificText.text =
+                            binding.mealNutritionalInfoSpecificText.text.toString() + it.first.name + " - " + it.second.toString() + "g\n"
+                    }
+                }
+                "Salt" -> {
+                    binding.mealNutritionalInfoSpecificText.text = e.value.toString() + "g total salt\n\n"
+                    saltList.forEach {
+                        binding.mealNutritionalInfoSpecificText.text =
+                            binding.mealNutritionalInfoSpecificText.text.toString() + it.first.name + " - " + it.second.toString() + "g\n"
+                    }
+                }
+            }
+
         }
     }
 
