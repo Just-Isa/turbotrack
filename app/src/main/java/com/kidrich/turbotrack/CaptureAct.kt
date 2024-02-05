186f51796bbd2165df672d53f192771d5a3f47d8
Isa Kilic <Is4Kilic@hotmail.com>
2024-02-05 22:11:15 +0100

    feature/ added flashlight controls

--- app/src/main/java/com/kidrich/turbotrack/CaptureAct.kt
+++ app/src/main/java/com/kidrich/turbotrack/CaptureAct.kt
@@ -1,7 +1,12 @@
 package com.kidrich.turbotrack
 
+import android.view.View
 import com.journeyapps.barcodescanner.CaptureActivity
+import com.journeyapps.barcodescanner.DecoratedBarcodeView
 
 class CaptureAct : CaptureActivity() {
-
+    override fun initializeContent(): DecoratedBarcodeView {
+        setContentView(R.layout.activity_capture)
+        return (findViewById<View>(R.id.zxing_view) as DecoratedBarcodeView)
+    }
 }
